f5a1678991350c96a204ca4377590fc661bf67b4
jblotus <jblotus@gmail.com>
2015-09-23 11:24:45 +0000

    remove console.log

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@ require(["jquery"], function($){
 		$(document).on('change','select', function(){
 			var id = $(this).val();
 			var template = $.trim($(id).html());
-			console.log($(this).val());
+			
 			var context  = function() {
 				var context =  $.trim($(id + '-context').html());				
 				return context ? context : '{}';
